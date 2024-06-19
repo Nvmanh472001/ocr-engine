98ec79647da4008b21dbc9d4e3b81aa76e40be89
lhnguyen4748 <lhnguyen4748@gmail.com>
2024-06-19 13:14:49 +0700

    add optimizer hesso for pruning step

--- optimizer/__init__.py
+++ optimizer/__init__.py
@@ -1,15 +1,27 @@
 import copy
+from typing import Any, Dict
 
 import torch
+import torch.nn as nn
+from only_train_once import OTO
 
 __all__ = ["build_optimizer"]
 
 
-def build_optimizer(optim_config, lr_scheduler_config, epochs, step_each_epoch, model):
+def build_optimizer(
+    optim_config: Dict[str, Any],
+    lr_scheduler_config: Dict[str, Any],
+    epochs: int,
+    step_each_epoch: int,
+    model: nn.Module | OTO,
+):
     from . import lr
 
-    config = copy.deepcopy(optim_config)
-    optim = getattr(torch.optim, config.pop("name"))(params=model.parameters(), **config)
+    optim_config = copy.deepcopy(optim_config)
+    if isinstance(model, OTO):
+        optim = model.hesso(variant=optim_config.pop("name").lower(), **optim_config)
+    else:
+        optim = getattr(torch.optim, optim_config.pop("name"))(params=model.parameters(), **optim_config)
 
     lr_config = copy.deepcopy(lr_scheduler_config)
     lr_config.update({"epochs": epochs, "step_each_epoch": step_each_epoch})
